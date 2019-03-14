4989fdc60717a48ef161c150265a1fdab826bb20
Dominick Geronimo <lorddom22@gmail.com>
2019-03-13 21:37:31 -0400

    stuff

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,116 +1,5 @@
-$(document).ready(function(){
-
-  
-  // Initialize and add the map
-   function initMap() {
-  // //   // The location of charlotte
-   var charlotte = {lat: 35.227085, lng: -80.843124};
-  //  // The map, centered at charlotte
-   var map = new google.maps.Map(
-   document.getElementById('map'), {zoom: 10, center: charlotte});
-  //  // The marker, positioned at charlotte
-   var marker = new google.maps.Marker({position: charlotte, map: map});
-  }
-  initMap()
-
-  var beer = "Ale"
-var brewery = "Brewery"
-var directions = "directions"
-  
-  $("#primary-search").click(function(){
-    event.preventDefault()
-  //  alert("hi")
-  //   $(".beer-buddy-title").fadeOut(7000)
-  //   $(".beer-search-container").fadeIn(10000)
-    //$(".beer-search-container").css({display: "block" })
-   $(".beer-search-container").css({display:"block"})
-   $(".beer-buddy-title").css({display:"none"})
-   $("#beer-search").css({display:"none"})
-   $("#primary-search").css({display:"none"})
-   $(".search-option ").css({display:"block"})
-   $(".nav-bar2-container").css({display:"block"})
-   $(".link-container").css({display:"none"})
-   $(".search-option ").css({top:"-165px"})
-   $("#buddy").css({position:"relative"})
-   $("#buddy").css({top:"-30px"})
-   $("#map").css({display:"block"})
-   $(".zip").css({display:"none"})
-// turn this into a function  out side of the scope the  click then place the function in click
-
-    pageTwo()
-
-
-  })
-
-
-
-  function pageTwo(){
-
-
-    $(".beer-search-container").html($("<div>", {class:"result" }))
-    $(".result").html($("<img>", {class:"beer-image" }))
-    $(".beer-image").after($("<div>", {class:"beer-name" }))
-    //anchor tags are missing links
-    
-  
-    var link = $("<a>");
-    link.attr("href", "#beer");
-    link.text(beer);
-    link.addClass("beer-link");
-
-    
-  
-    $(".beer-name").html(link);
-   // $(".beer-name").html($("<a>", {class:"beer-link" }))
-   // the ol elements need to be AFTER to the div then the first li need to be inside but the following need to be after()
-   $(".beer-link").after($("<ul>", {class:"list" }))
-  
-  
-   
-   var li = $("<li>");
-    li.text(brewery)
-    li.addClass("list-one");
-    $(".list").html(li)
-
-  var  li2 = $("<li>");
-    li2.text(brewery)
-    li2.addClass("list-two");
-    $(".list-one").after(li2)
-
-    var  li3 = $("<li>");
-    li3.text(brewery)
-    li3.addClass("list-three");
-    $(".list-two").after(li3)
-   //$(".list").html($("<li>", {class:"list-catorgorey" }))
-   //$(".list-catorgorey").html({text:brewery })
-   
-  
-   //$(".list-catorgorey").after($("<li>",{text:brewery }))
-   //$(".list-catorgorey").after($("<li>",{text:brewery }))
-   //$(".list-catorgorey").after($("<li>",{class:"beer-link2" }))
-  
-   
-  
-   var link2 = $("<a>");
-    link2.attr("href", directions);
-    link2.text(directions);
-    //link2.addClass("beer-link2");
-  
-    $(".list-three").html(link2);
-   // li below doesn't work correctly anchor is placed inside but cannot add attribute
-   //$(".list-catorgorey").after($("<li>"+ "<a>", {href:"list-catorgorey" } + "</a>" + "</li>"))
-   
-    
-  
-  
-  
-  
-   // works but li elements are empty $(".list").append($("<li>" + "name")).append("<li>").append("<li>" ).append("<li>")
-    
-  
-  }
-})
-
+//TODO: reconcile variable names for functions and displays
+//TODO: Link variable information
 
 // variable names
 var beerName;
@@ -125,10 +14,7 @@ var website;
 var breweryAddress;
 var breweryOpen
 var breweryRegion
-
-
 //TODO: create a dropdown or other selector to determine which below function to run
-
 
 // Initialize and add the map
 function initMap() {
@@ -142,42 +28,12 @@ function initMap() {
 
 }
 
-
-
 // this function will search for and return information on a beer entered
 function callBeer() {
 
   var queryURL = "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/search";
   var search = $("#beer-search").val();
 
-<<<<<<< HEAD
-$.ajax({
-  url: queryURL,
-  method: "GET",
-  data: {
-    q: search,
-    type: "beer",
-    withBreweries: "Y",
-    withLocations: "Y",
-    key: config,
-  } 
-}).then(function (response) {
-  console.log(response)
-  var result = response.data;
-  
-  for (var i = 0; i < 10; i++){
-    beerName = result[i].name;
-    breweryName = result[i].breweries[0].name;
-    brewLat = result[i].breweries[0].locations[0].latitude;
-    brewLng = result[i].breweries[0].locations[0].longitude;
-    abv = result[i].abv;
-    style = result[i].style.name;
-    ibu = result[i].ibu;
-    website = result[i].breweries[0].website;
-
-    initiMap();
-  }
-=======
   $.ajax({
     url: queryURL,
     method: "GET",
@@ -199,7 +55,7 @@ $.ajax({
       });
 
 
-    for (var i = 0; i <= 0; i++) {
+    for (var i = 0; i < 10; i++) {
       beerName = result[i].name;
       breweryName = result[i].breweries[0].name;
       brewLat = result[i].breweries[0].locations[0].latitude;
@@ -210,45 +66,20 @@ $.ajax({
       website = result[i].breweries[0].website;
       //placed new markers
       var marker = new google.maps.Marker({ position: { lat: brewLat, lng: brewLng }, map: map });
-
+      //TODO: Dynamically create display card 
+      pageTwo();
     }
->>>>>>> 82e4fe3dee2049bfea024bb1d15e5a7030965810
 
   });
 
 };
 
-<<<<<<< HEAD
-  //This function takes a postal code and returns breweries within a 10 mile radius
-  function callLocation() {
-  
-    var queryURL =  "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/locations";
-    var search = $("#beer-search").val();
-  
-=======
-//TODO: move the below comment to where we want to call the beer search function
-// callBeer();
-
-
-
-// Initialize and add the map for new function
-function initMap() {
-  console.log("Homey!");
-
-  // The location of kansas
-  var breweryMapData = { lat: 39.0119, lng: -98.4842 };
-  // The map, centered at kansas
-  var map = new google.maps.Map(
-    document.getElementById('map'), { zoom: 3.5, center: breweryMapData });
-
-}
 //This function takes a postal code and returns breweries within a 10 mile radius
 function callLocation() {
 
   var queryURL = "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/locations";
   var search = $("#beer-search").val();
 
->>>>>>> 82e4fe3dee2049bfea024bb1d15e5a7030965810
   $.ajax({
     url: queryURL,
     method: "GET",
@@ -277,34 +108,14 @@ function callLocation() {
       } else {
         console.log("Closed: " + breweryName)
       }
-      
+
     }
 
-<<<<<<< HEAD
-    //This function calls a random beer, then stores its name as a variable to run through the GET beer search function
-  function callRandom (){
-    var queryURL =  "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/beer/random";
-=======
   });
 
 };
 
-//TODO: place this where we want the location function to be called.
-// callLocation();
 
-
-
-// Initialize and add the map for new function
-function initMap() {
-  console.log("Dude!");
-
-  // The location of kansas
-  var breweryMapData = { lat: 39.0119, lng: -98.4842 };
-  // The map, centered at kansas
-  var map = new google.maps.Map(
-    document.getElementById('map'), { zoom: 3.5, center: breweryMapData });
-
-}
 //This function calls a random beer, then stores its name as a variable to run through the GET beer search function
 function callRandom() {
   var queryURL = "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/beer/random";
@@ -322,7 +133,6 @@ function callRandom() {
     beerName = result.name;
 
     var queryURL = "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/search";
->>>>>>> 82e4fe3dee2049bfea024bb1d15e5a7030965810
 
     $.ajax({
       url: queryURL,
@@ -358,21 +168,82 @@ function callRandom() {
 
 };
 
+var beer = "Ale"
+var brewery = "Brewery"
+var directions = "directions"
 
-  //TODO: place this where we want the location function to be called.
-   // callLocation();
+$("#primary-search").click(function () {
+  event.preventDefault()
+  //TODO: Input verification
+  //TODO: create attribute or value to differentiate between zip code entry and beer entry
 
-  //TODO: move the below comment to where we want to call the beer search function
-  // callBeer();
+  if (createdAttribute === "beer") {
+    callBeer();
+  } else if (createdAttribute === "zip") {
+    callLocation();
+  }
 
-  //TODO: Place this on a random beer search button
-  // callRandom();
+  //TODO: Ensure the below code dynamically creates what we want
+  $(".beer-search-container").css({ display: "block" })
+  $(".beer-buddy-title").css({ display: "none" })
+  $("#beer-search").css({ display: "none" })
+  $("#primary-search").css({ display: "none" })
+  $(".search-option ").css({ display: "block" })
+  $(".nav-bar2-container").css({ display: "block" })
+  $(".link-container").css({ display: "none" })
+  $(".search-option ").css({ top: "-165px" })
+  $("#buddy").css({ position: "relative" })
+  $("#buddy").css({ top: "-30px" })
+  $("#map").css({ display: "block" })
+  $(".zip").css({ display: "none" })
+})
 
 
 
-<<<<<<< HEAD
-  
-=======
+function pageTwo() {
+  //TODO: ENsure this creates 10 cards as per For loop callback
+  //TODO: Only one link to var website
+
+  $(".beer-search-container").html($("<div>", { class: "result" }))
+  $(".result").html($("<img>", { class: "beer-image" }))
+  $(".beer-image").after($("<div>", { class: "beer-name" }))
+  //anchor tags are missing links
 
 
->>>>>>> 82e4fe3dee2049bfea024bb1d15e5a7030965810
+  var link = $("<a>");
+  link.attr("href", "#beer");
+  link.text(beer);
+  link.addClass("beer-link");
+
+
+
+  $(".beer-name").html(link);
+  $(".beer-link").after($("<ul>", { class: "list" }))
+
+
+
+  var li = $("<li>");
+  li.text(brewery)
+  li.addClass("list-one");
+  $(".list").html(li)
+
+  var li2 = $("<li>");
+  li2.text(brewery)
+  li2.addClass("list-two");
+  $(".list-one").after(li2)
+
+  var li3 = $("<li>");
+  li3.text(brewery)
+  li3.addClass("list-three");
+  $(".list-two").after(li3)
+
+  var link2 = $("<a>");
+  link2.attr("href", directions);
+  link2.text(directions);
+
+  $(".list-three").html(link2);
+}
+
+
+//TODO: Create on click event for user to get random beer
+//Submit button "on click" callRandom();
