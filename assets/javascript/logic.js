c36103bbbc64837ff35a76b6188ae86014fe2b09
yashua <yashuab.abraham@gmail.com>
2019-03-16 05:58:15 -0400

    added text verification, removed link styling, linked home page to title, work on page two function

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -13,6 +13,8 @@ var website;
 var breweryAddress;
 var breweryOpen
 var breweryRegion
+
+
 //TODO: create a dropdown or other selector to determine which below function to run
 
 // Initialize and add the map
@@ -107,13 +109,13 @@ function callLocation() {
       marker = new google.maps.Marker({ position: { lat: brewLat, lng: brewLng }, map: map });
       pageTwo();
 
-      // if (breweryOpen === "Y") {
-      //   console.log("Open: " + breweryName)
-      //   var marker = new google.maps.Marker({ position: { lat: brewLat, lng: brewLng }, map: map });
-      //   pageTwo();
-      // } else {
-      //   console.log("Closed: " + breweryName)
-      // }
+       if (breweryOpen === "Y") {
+         console.log("Open: " + breweryName)
+         var marker = new google.maps.Marker({ position: { lat: brewLat, lng: brewLng }, map: map });
+         pageTwo();
+       } else {
+         console.log("Closed: " + breweryName)
+       }
 
     }
 
@@ -165,7 +167,7 @@ function callRandom() {
         website = result[i].breweries[0].website;
         //new markers added
         marker = new google.maps.Marker({ position: { lat: brewLat, lng: brewLng }, map: map });
-        pageTwo();
+       // pageTwo();
       }
 
     });
@@ -174,11 +176,54 @@ function callRandom() {
 
 };
 
+// function theTridentOfPoseidon() {
+
+//   for (var i=0; i <10; i++) {
+//     $(".beer-search-container").append("<tr>")
+//       .append("<td>" + beerName + "</td>")
+//       .append("<td>" + breweryName + "</td>")
+//       .append("<td>" + style + "</td>")
+//       .append("<td>" + abv + "</td>")
+//       .append("<td>" + ibu + "</td>")
+//       .append("<td>" + breweryAddress + "</td>")
+//   }
+
+// };
+
+function createContainer(){
+
+  var displayDivCon = '<div class="result">' +
+  '<img class="beer-image"> ' +
+  '<div class="beer-name">' + '<a href="#beer">Beer Name</a>' + '</div>' +
+  '<ol>' +
+    '<li>Brewery</li>' +
+    '<li>Style</li>' +
+    '<li>beer :</li>' +
+    '<li>' +'<a href="#directions">Directions</a>'+'</li>' +
+  
+  '</ol>' +
+  
+  '</div>'
+
+  var displayDivCon2  = $(".beer-search-container").append(displayDivCon)
+
+  displayDivCon2 .append(displayDivCon)
+
+
+}
+
+
+
+
 $("#primary-search").click(function () {
   event.preventDefault()
   //TODO: Input verification
   //TODO: create attribute or value to differentiate between zip code entry and beer entry
+  
+  var value = $("#beer-search").val()
+  var inputVal = $.isNumeric(value)
   var searchParameter = $('.dropdown').val();
+  console.log(value)
 
   if (searchParameter === "random") {
     callRandom();
@@ -190,7 +235,17 @@ $("#primary-search").click(function () {
     callLocation();
     console.log(searchParameter)
   }
-
+    console.log(inputVal)
+    console.log(value.length)
+  if (value.length > 5 && inputVal === true ){
+    $("#beer-search").val("")
+    $("#beer-search").attr("placeholder", "Not a valid zipcode! Please enter a correct zipcode")
+    
+    return false;
+   // alert("Not valid Zipcode")
+  }
+  
+  // if{searchParameter === }
   //TODO: Ensure the below code dynamically creates what we want
   $(".beer-search-container").css({ display: "block" })
   $(".beer-buddy-title").css({ display: "none" })
@@ -199,17 +254,23 @@ $("#primary-search").click(function () {
   $(".search-option ").css({ display: "block" })
   $(".nav-bar2-container").css({ display: "block" })
   $(".link-container").css({ display: "none" })
-  $(".search-option ").css({ top: "-20px" })
+  $(".search-option ").css({ bottom: "17px" })
   $("#buddy").css({ position: "relative" })
-  $("#buddy").css({ top: "-14px" })
+  $("#buddy").css({ bottom: "13px" })
   $("#map").css({ display: "block" })
   $(".zip").css({ display: "none" })
   $(".dropdown").css({ display: "none" })
+createContainer()
+ //theTridentOfPoseidon()
 })
 
-function pageTwo() {
-  //TODO: ENsure this creates 10 cards as per For loop callback
-  //TODO: Only one link to var website
+// function pageTwo() {
+
+
+
+
+//   //TODO: ENsure this creates 10 cards as per For loop callback
+//   //TODO: Only one link to var website
 
   $(".beer-search-container").html($("<div>", { class: "result" }))
   $(".result").html($("<img>", { class: "beer-image" }))
@@ -264,5 +325,5 @@ function pageTwo() {
   link2.text(website);
 
   $(".list-three").html(link2);
-}
+//}
 
